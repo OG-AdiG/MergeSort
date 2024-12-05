473abeb4c51d355b2889497524ad3c8c2323e216
OG-AdiG <adriangargula@wp.pl>
2024-12-05 05:08:38 +0100

    testy v2

--- MergeSortTest/test.cpp
+++ MergeSortTest/test.cpp
@@ -46,3 +46,94 @@ TEST(Tests, negativepositiveOrder) {
     ASSERT_EQ(t, expected);
 
 }
+
+TEST(Tests, empty) {
+
+    std::vector<int> t = {};
+    MergeSort::Sort(t);
+    std::vector<int> expected = {};
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, onenumb) {
+
+    std::vector<int> t = { 34 };
+    MergeSort::Sort(t);
+    std::vector<int> expected = { 34 };
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, duplicates) {
+
+    std::vector<int> t = { 2, 1, 3, 8, 1 ,8 };
+    MergeSort::Sort(t);
+    std::vector<int> expected = { 1, 1, 3, 3, 6 ,6 };
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, negativeDuplicates) {
+
+    std::vector<int> t = { -2, -1, -2, -8, -1 ,-8 };
+    MergeSort::Sort(t);
+    std::vector<int> expected = { -8, -8, -2, -2, -1, -1 };
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, twonumb) {
+
+    std::vector<int> t = { 1,2 };
+    MergeSort::Sort(t);
+    std::vector<int> expected = { 1,2 };
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, hundred) {
+
+    std::vector<int> t(101);
+    std::vector<int> expected(101);
+
+    for (int i = 0; i < 101; i++) {
+
+        expected[i] = i * 2;
+
+    }
+
+    for (int i = 0; i < 101; i++) {
+
+        t[i] = i * 2;
+
+    }
+
+    t[0] = 2;
+    t[1] = 0;
+    MergeSort::Sort(t);
+    ASSERT_EQ(t, expected);
+
+}
+
+TEST(Tests, hundredmix) {
+
+    std::vector<int> t(201);
+    std::vector<int> expected(201);
+
+    for (int i = -100; i < 101; i++) {
+
+        int index = i + 100;
+        expected[index] = i * 2;
+        t[index] = i * 2;
+    }
+
+    t[20] = 16;
+    expected[20] = 16;
+    t[20] = 6;
+    expected[20] = 6;
+    std::sort(expected.begin(), expected.end());
+    MergeSort::Sort(t);
+    ASSERT_EQ(t, expected);
+
+}
